576d44b800a5789b5f2d8786ff92fd10740800b2
ZhuMingYanMIA <zhuming_yan@163.com>
2025-03-14 16:22:36 +0800

    Add files via upload

---
+++ PEMS07.py
@@ -0,0 +1,154 @@
+import os
+import sys
+import random
+
+# TODO: remove it when basicts can be installed by pip
+sys.path.append(os.path.abspath(__file__ + "/../../.."))
+import torch
+from easydict import EasyDict
+from CMH_arch import CMH
+from CMH_runner import CMHRunner
+from CMH_data import ForecastingDataset
+from basicts.losses import masked_mae
+from basicts.utils.dataloader import load_adj_from_numpy
+from basicts.utils.graph_algo import normalize_adj_mx
+from basicts.utils import load_adj
+CFG = EasyDict()
+
+CFG = EasyDict()
+
+# ================= general ================= #
+CFG.DESCRIPTION = "(PEMS07) configuration"
+CFG.RUNNER = CMHRunner
+CFG.DATASET_CLS = ForecastingDataset
+CFG.DATASET_NAME = "PEMS07"
+CFG.DATASET_TYPE = "Traffic flow"
+CFG.DATASET_INPUT_LEN = 12
+CFG.DATASET_OUTPUT_LEN = 12
+CFG.DATASET_ARGS = {
+    "seq_len": 288
+    }
+CFG.GPU_NUM = 1
+
+# ================= environment ================= #
+CFG.ENV = EasyDict()
+CFG.ENV.SEED =  random.randint(0,10000000)
+CFG.ENV.CUDNN = EasyDict()
+CFG.ENV.CUDNN.ENABLED = True
+
+# ================= model ================= #
+CFG.MODEL = EasyDict()
+CFG.MODEL.NAME = "CMH"
+CFG.MODEL.ARCH = CMH
+adj_mx, _ = load_adj("datasets/" + CFG.DATASET_NAME + "/adj_mx.pkl", "doubletransition")
+CFG.MODEL.PARAM = {
+    "dataset_name": CFG.DATASET_NAME,
+    "pre_trained_tmae_path": "mask_save/TMAE_PEMS07_864.pt",
+    "pre_trained_smae_path": "mask_save/SMAE_PEMS07_864.pt",
+    "mask_args": {
+                    "patch_size":12,
+                    "in_channel":1,
+                    "embed_dim":96,
+                    "num_heads":4,
+                    "mlp_ratio":4,
+                    "dropout":0.1,
+                    "mask_ratio":0.25,
+                    "encoder_depth":4,
+                    "decoder_depth":1,
+                    "mode":"forecasting"
+    },
+    "backend_args": {
+                    "num_nodes": 883,
+                    "supports": [torch.tensor(i) for i in adj_mx],
+                    "input_len": 12,
+                    "input_dim": 3,
+                    "if_T_i_D": True,
+                    "if_D_i_W": True,
+                    "temp_dim_tid": 32,
+                    "temp_dim_diw": 32,
+                    "time_of_day_size": 288,
+                    "day_of_week_size": 7,
+                    "output_len": 12,
+                    "num_layer": 3,
+                    "if_node": True,
+                    "node_dim": 32,
+                    "node_hidden": 64,
+                    "embed_dim": 32,
+                    "nheah": 2,
+                    "fusion_dim": 64
+    }
+}
+CFG.MODEL.FROWARD_FEATURES = [0,1,2]
+CFG.MODEL.TARGET_FEATURES = [0]
+CFG.MODEL.DDP_FIND_UNUSED_PARAMETERS = True
+
+# ================= optim ================= #
+CFG.TRAIN = EasyDict()
+CFG.TRAIN.LOSS = masked_mae
+CFG.TRAIN.OPTIM = EasyDict()
+CFG.TRAIN.OPTIM.TYPE = "Adam"
+CFG.TRAIN.OPTIM.PARAM= {
+    "lr":0.002,
+    "weight_decay":1.0e-5,
+    "eps":1.0e-8,
+}
+CFG.TRAIN.LR_SCHEDULER = EasyDict()
+CFG.TRAIN.LR_SCHEDULER.TYPE = "MultiStepLR"
+CFG.TRAIN.LR_SCHEDULER.PARAM= {
+    "milestones":[1, 18, 36, 54, 72],
+    "gamma":0.5
+}
+
+# ================= train ================= #
+CFG.TRAIN.CLIP_GRAD_PARAM = {
+    "max_norm": 3.0
+}
+CFG.TRAIN.NUM_EPOCHS = 300
+
+CFG.TRAIN.CKPT_SAVE_DIR = os.path.join(
+    "checkpoints",
+    "_".join([CFG.MODEL.NAME, str(CFG.TRAIN.NUM_EPOCHS)])
+)
+# train data
+CFG.TRAIN.DATA = EasyDict()
+CFG.TRAIN.NULL_VAL = 0.0
+# read data
+CFG.TRAIN.DATA.DIR = "datasets/" + CFG.DATASET_NAME
+# dataloader args, optional
+CFG.TRAIN.DATA.BATCH_SIZE = 32
+CFG.TRAIN.DATA.PREFETCH = False
+CFG.TRAIN.DATA.SHUFFLE = True
+CFG.TRAIN.DATA.NUM_WORKERS = 9
+CFG.TRAIN.DATA.PIN_MEMORY = True
+CFG.TRAIN.CL = EasyDict()
+CFG.TRAIN.CL.WARM_EPOCHS = 0
+CFG.TRAIN.CL.CL_EPOCHS = 6
+CFG.TRAIN.CL.PREDICTION_LENGTH = 12
+# ================= validate ================= #
+CFG.VAL = EasyDict()
+CFG.VAL.INTERVAL = 1
+# validating data
+CFG.VAL.DATA = EasyDict()
+# read data
+CFG.VAL.DATA.DIR = "datasets/" + CFG.DATASET_NAME
+# dataloader args, optional
+CFG.VAL.DATA.BATCH_SIZE = 8
+CFG.VAL.DATA.PREFETCH = False
+CFG.VAL.DATA.SHUFFLE = False
+CFG.VAL.DATA.NUM_WORKERS = 9
+CFG.VAL.DATA.PIN_MEMORY = True
+
+# ================= test ================= #
+CFG.TEST = EasyDict()
+CFG.TEST.INTERVAL = 1
+# evluation
+# test data
+CFG.TEST.DATA = EasyDict()
+# read data
+CFG.TEST.DATA.DIR = "datasets/" + CFG.DATASET_NAME
+# dataloader args, optional
+CFG.TEST.DATA.BATCH_SIZE = 64
+CFG.TEST.DATA.PREFETCH = False
+CFG.TEST.DATA.SHUFFLE = False
+CFG.TEST.DATA.NUM_WORKERS = 9
+CFG.TEST.DATA.PIN_MEMORY = True
